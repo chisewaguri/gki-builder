e54f5e15714ed7fb7f089be08ff29c27b8c236c4
chise <lqff.yt@gmail.com>
2025-10-07 11:23:27 +0700

    personal config: melt

--- config.sh
+++ config.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # Kernel name
-KERNEL_NAME="Glow-Chise-v3.9+"
+KERNEL_NAME="android12-9-00064-gea4a6f067d3f-ab1296991"
 
 # GKI Version
 GKI_VERSION="android12-5.10"
 
 # Build variables
-export TZ="Asia/Makassar"
-export KBUILD_BUILD_USER="eraselk"
-export KBUILD_BUILD_HOST="$KERNEL_NAME"
-export KBUILD_BUILD_TIMESTAMP=$(date)
+export TZ="$TZ"
+export KBUILD_BUILD_USER="build-user"
+export KBUILD_BUILD_HOST="build_host"
+export KBUILD_BUILD_TIMESTAMP="Fri Jan 24 18:48:47 UTC 2025"
 
 # AnyKernel variables
 ANYKERNEL_REPO="https://github.com/hazepynut/anykernel"
